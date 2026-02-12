38746ae183cfa1624f8694c398729aea83655df5
Prajita Sanyasi <psanyasi@academic.rrc.ca>
2026-02-12 13:17:05 -0600

    Created functions for addition and multiplication.

---
+++ pj-script.js
@@ -0,0 +1,18 @@
+/*
+Prajita Sanyasi
+Feb 12, 2025
+Addition + Multiplication
+*/
+const number1 = document.getElementById("number1");
+const number2 = document.getElementById("number2");
+
+
+function addition{
+    addition_calculation => number1 + number2;
+    getElementById("result-value").textContext = addition_calculation
+}
+
+function multiplication{
+    multiplication_calculation => number1 * number2;
+    getElementById("result-value").textContext = multiplication_calculation;
+}
